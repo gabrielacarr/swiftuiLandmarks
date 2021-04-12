38fa5d9dfef4235710f31524b10a72573f86d73d
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 16:59:57 -0400

    Update: Render Navigation Between List + Detail

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -10,11 +10,16 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+                    List(landmarks) { landmark in
+                        NavigationLink(destination: LandmarkDetail()) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+                    .navigationTitle("Landmarks")
+                }
+            }
         }
-    }
-}
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
