d34c20db1175432c3d281405fac9b606a7aef2d0
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 14:48:21 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Brie New on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
