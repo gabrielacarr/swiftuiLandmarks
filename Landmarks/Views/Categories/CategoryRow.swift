fd2f62860e7830fc1c604daa46be781b305ff4c9
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 19:52:58 -0400

    Create: Category Row + display names

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Brie New on 4/12/21.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+
+    var body: some View {
+        Text(categoryName)
+            .font(.headline)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(3))
+        )
+    }
+}
