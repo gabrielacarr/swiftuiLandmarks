975b0fe1cc42766d63ab0eaafc014b137cbb8d08
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 19:10:39 -0400

    Fix: Size of badge symbol geometry + scale

--- Landmarks/Badges/Badge.swift
+++ Landmarks/Badges/Badge.swift
@@ -14,13 +14,18 @@ struct Badge: View {
     }
 
     var body: some View {
-        ZStack {
-            BadgeBackground()
+            ZStack {
+                BadgeBackground()
 
-            badgeSymbols
+                GeometryReader { geometry in
+                    badgeSymbols
+                        .scaleEffect(1.0 / 4.0, anchor: .top)
+                        .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+                }
+            }
         }
     }
-}
+
 
 struct Badge_Previews: PreviewProvider {
     static var previews: some View {
