6165cc66c489f65c9bbebbee6ff0f5a6e2bff5af
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 19:54:49 -0400

    Update: Cat items in HStack + cat name in VStack

--- Landmarks/Views/Categories/CategoryRow.swift
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -12,8 +12,21 @@ struct CategoryRow: View {
     var items: [Landmark]
 
     var body: some View {
-        Text(categoryName)
-            .font(.headline)
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        Text(landmark.name)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
     }
 }
 
@@ -23,7 +36,7 @@ struct CategoryRow_Previews: PreviewProvider {
     static var previews: some View {
         CategoryRow(
             categoryName: landmarks[0].category.rawValue,
-            items: Array(landmarks.prefix(3))
+            items: Array(landmarks.prefix(4))
         )
     }
 }
