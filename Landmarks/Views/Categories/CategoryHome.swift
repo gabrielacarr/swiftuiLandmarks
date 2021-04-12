0b33e705d461520935fb3d610f4722bb5075632f
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 19:41:10 -0400

    Add: Category view + navigation

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Brie New on 4/12/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+                   Text("Hello, World!")
+                       .navigationTitle("Featured")
+               }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
