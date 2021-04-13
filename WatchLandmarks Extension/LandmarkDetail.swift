e778bc698ab2b4caf372129bfc66aeaa7faf7ef4
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 21:59:51 -0400

    Update: Circle view render + Fix: Scroll view on watch

--- WatchLandmarks Extension/LandmarkDetail.swift
+++ WatchLandmarks Extension/LandmarkDetail.swift
@@ -15,14 +15,43 @@ struct LandmarkDetail: View {
     }
 
     var body: some View {
-        Text("Hello, World!")
-    }
-}
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFill()
 
-struct LandmarkDetail_Previews: PreviewProvider {
-    static var previews: some View {
-        let modelData = ModelData()
-        return LandmarkDetail(landmark: modelData.landmarks[0])
-            .environmentObject(modelData)
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+
+                Divider()
+
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+
+                Text(landmark.state)
+                    .font(.caption)
+            }
+        }
     }
 }
+    struct LandmarkDetail_Previews: PreviewProvider {
+        static var previews: some View {
+            let modelData = ModelData()
+            return Group {
+                LandmarkDetail(landmark: modelData.landmarks[0])
+                    .environmentObject(modelData)
+                    .previewDevice("Apple Watch Series 5 - 44mm")
+
+                LandmarkDetail(landmark: modelData.landmarks[1])
+                    .environmentObject(modelData)
+                    .previewDevice("Apple Watch Series 5 - 40mm")
+            }
+        }
+    }
