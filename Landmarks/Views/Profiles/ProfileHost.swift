8ea4c24ef01f25abed4f7cb5e93ed60053be4d88
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 20:20:37 -0400

    Update: Profile display new summary view

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -11,9 +11,12 @@ struct ProfileHost: View {
     @State private var draftProfile = Profile.default
 
     var body: some View {
-        Text("Profile for: \(draftProfile.username)")
+            VStack(alignment: .leading, spacing: 20) {
+                ProfileSummary(profile: draftProfile)
+            }
+            .padding()
+        }
     }
-}
 
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
