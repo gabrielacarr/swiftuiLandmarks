8dd5b24f787a997256ec8ec8aaae3a0a96de5b90
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 20:53:36 -0400

    Update: Place picker + date picker with notification

--- Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -17,12 +17,25 @@ struct ProfileEditor: View {
                 Divider()
                 TextField("Username", text: $profile.username)
             }
-                        Toggle(isOn: $profile.prefersNotifications) {
-                            Text("Enable Notifications").bold()
-                        }
+
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notifications").bold()
+            }
+
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Seasonal Photo").bold()
+
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
                     }
                 }
+                .pickerStyle(SegmentedPickerStyle())
             }
+        }
+    }
+}
+
 struct ProfileEditor_Previews: PreviewProvider {
     static var previews: some View {
         ProfileEditor(profile: .constant(.default))
