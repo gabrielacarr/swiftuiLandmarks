1db9e1dadf5441e8088c46dfcfd75e2bccd47f18
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-12 20:27:26 -0400

    Update: Profile view to render button

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
 
     var body: some View {
         NavigationView {
@@ -21,11 +22,22 @@ struct CategoryHome: View {
                     .listRowInsets(EdgeInsets())
 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
-                                }
-                                .listRowInsets(EdgeInsets())
-                            }
-                            .navigationTitle("Featured")
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .listStyle(InsetListStyle())
+            .navigationTitle("Featured")
+            .toolbar {
+                Button(action: { showingProfile.toggle() }) {
+                    Image(systemName: "person.crop.circle")
+                        .accessibilityLabel("User Profile")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
